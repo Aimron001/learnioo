3b20503bf15cdeed35d1ab05a911b45fd40ad3f1
Ronald Mutua <ronaldmutua617@gmail.com>
2024-03-27 12:24:01 +0300

    configured the login page to user the login reducers

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import FormInput from "../../components/formInput/FormInput";
 import "./login.css"
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLoginMutation } from '../../features/api/userApiSlice';
+import { setCredentials } from '../../features/auth/authSlice';
 import {faEnvelope, faLock} from '@fortawesome/free-solid-svg-icons'
+import { toast } from 'react-toastify'
+
 
 export default function Login(){
     const [loginData, setLoginData] = useState({email:"",password:""})
 
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const [login, { isLoading }] = useLoginMutation()
+
+    const { userInfo } = useSelector((state) => state.auth)
+
+    useEffect(()=>{
+        if (userInfo) {
+            navigate('../workspace')
+        }
+
+    }, [navigate, userInfo])
     function handleChange(e){
         setLoginData(prevData => ({
             ...prevData,
@@ -36,6 +54,14 @@ export default function Login(){
 
     async function handleSubmit(e){
         e.preventDefault()
+        try {
+            const res = await login({...loginData}).unwrap();
+            dispatch(setCredentials({...res}))
+            navigate('../workspace')
+        } catch (err) {
+            toast.error(err?.data?.message || err.error);
+            console.log(err);
+        }
     }
     return (
         <form className="login-container" onSubmit={handleSubmit}>
@@ -54,7 +80,7 @@ export default function Login(){
                 />
             })}
             </div>
-            <button className="btn-login" >Login</button>
+            <button className="btn-login" disabled={isLoading}>Login</button>
             <div className='account'>
                 <p>Don`&apos;`t have an account?</p>
                 <Link to="../register">Sign Up</Link>
