2e8f6258114133f62e71af680cb5e4ed26652f4f
Ronald Mutua <ronaldmutua617@gmail.com>
2024-03-27 11:46:25 +0300

    created the register route controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,7 +31,37 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
+    const { firstname, lastname, email, phoneNumber, password } = req.body
   
+    const userExists = await User.findOne({ email });
+  
+    if (userExists) {
+      res.status(400);
+      throw new Error("User already exists")
+  
+    }
+  
+    const user = await User.create({
+      firstname,
+      lastname,
+      email,
+      phoneNumber,
+      password,
+    })
+  
+    if (user) {
+      generateToken(res, user._id)
+      res.status(201).json({
+        id: user._id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        phoneNumber: user.phoneNumber,
+        email: user.email,
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+    }
 });
 
 // @desc    Logout user / clear cookie
