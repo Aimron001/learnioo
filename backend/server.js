15818ea73343cfd29aa1138142815b08b0d2918e
Ronald Mutua <ronaldmutua617@gmail.com>
2024-03-27 12:03:07 +0300

    setting up cors to allow frontend to access backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,33 @@
 import express from "express"
+import cors from "cors"
 import dotenv from "dotenv"
 import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+import connectDB from './config/db.js'
+import bookRoutes from './routes/bookRoutes.js'
+import cookieParser from "cookie-parser"
 dotenv.config()
 
 connectDB()
 const port = process.env.PORT || 8000
 const app = express()
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 
+  };
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
+app.use("/uploads", express.static('../uploads'));
 app.use('/api/users', userRoutes)
+app.use('/api/books', bookRoutes)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
+app.use(notFound)
+app.use(errorHandler)
